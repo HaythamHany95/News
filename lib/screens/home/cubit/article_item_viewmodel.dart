e57e57d261db63d7d50c426f2dde96e8716e6503
HaythamHany95 <haythamhany95@gmail.com>
2024-03-30 00:57:07 +0200

    Add ArticleItemViewModel and ArticleStates classes
    Added ArticleItemViewModel class to handle article state management and
    fetching news articles by source ID. Also added ArticleStates abstract class
    along with ArticleLoadingState, ArticleErrorState, and ArticleSuccessState
    classes to represent different states of the article fetching process.

---
+++ lib/screens/home/cubit/article_item_viewmodel.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/api/api_manager.dart';
+import 'package:news_app/screens/home/states/article_states.dart';
+
+class ArticleItemViewModel extends Cubit<ArticleStates> {
+  ArticleItemViewModel() : super(ArticleLoadingState());
+  void getNewsArticlesBySourceId(String? sourceId) async {
+    emit(ArticleLoadingState());
+    try {
+      var respose = await ApiManager.getNewsArticles(sourceId ?? "");
+      if (respose?.status == 'error') {
+        emit(ArticleErrorState(errorMessage: respose?.message));
+      }
+      if (respose?.status == 'ok') {
+        emit(ArticleSuccessState(articles: respose?.articles ?? []));
+      }
+    } catch (e) {
+      emit(ArticleErrorState(errorMessage: "Something went wrong"));
+    }
+  }
+}
